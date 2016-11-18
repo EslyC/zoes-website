b1825f3053f6b173cffa6ff8a134d10423bb55cf
Zoe Sobin <zsobin@hubspot.com>
2016-11-18 13:42:37 +0000

    remove unused dep

--- src/js/components/Projects.js
+++ src/js/components/Projects.js
@@ -12,21 +12,21 @@ const Projects = React.createClass({
           <p>Disclaimer: I built none of these</p>
         </div>
         <div className="container">
-          <div className="code-container">
+          <div className="project-container">
             <img src={facebook} alt="Flarpbook" />
             <div>
               <h3>Flarpbook</h3>
               <p>What a wild ride it's been. I build a social platform to help people connect and it's been a lot of fun to make a bajillion dollars. I learned a lot about trust and programming.</p>
             </div>
           </div>
-          <div className="code-container">
+          <div className="project-container">
             <img src={spotify} alt="Spootify"/>
             <div>
               <h3>Spootify</h3>
               <p>Everyone's favorite mystical music app. I learned so much about desktop applications and also making a product that lots of people like to get really aggressive about when they compare it to lesser services like Rdio (rip). Spootify changed the whole world and make iTunes look like trash and also saved people a lot of money and time when it got them to stop downloading music.</p>
             </div>
           </div>
-          <div className="code-container">
+          <div className="project-container">
             <img src={google} alt="Gargle"/>
             <div>
               <h3>Gargle</h3>
